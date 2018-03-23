53245f69670650bf5fa0abf50ec11d6ecd9cd425
Jade Rickerts <jade@code-beta.com>
2018-03-23 08:18:32 +0200

    add try and catch statements to main menu form

--- Order App/Form1.cs
+++ Order App/Form1.cs
@@ -17,18 +17,32 @@ namespace Order_App
         string uid = Properties.Settings.Default["UserID"].ToString();
         string pwd = Properties.Settings.Default["Password"].ToString();
 
-        //FORM INITIALIZATION WITH NO VARIABLES
+        //FORM INITIALIZATION WITH NO PARAMETERS
         public MainMenu()
         {
-            InitializeComponent();
-            startup = true;           
+            try
+            {
+                InitializeComponent();
+                startup = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Main Menu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }           
         }
 
         //FORM INITIALIZATION WITH START BOOL
         public MainMenu(bool start)
         {
-            InitializeComponent();
-            startup = start;
+            try
+            {
+                InitializeComponent();
+                startup = start;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Main Menu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         //CHECK FOR STOCK FILE UPDATE METHOD
@@ -76,18 +90,24 @@ namespace Order_App
         //MAIN MENU FORM STARTUP CONFIG
         private void MainMenu_Load(object sender, EventArgs e)
         {
-            if (startup == true && Convert.ToBoolean(Properties.Settings.Default["CheckForUpdates"]) == true)
+            try
             {
-                checkUpdate();
-            }
-            if (Properties.Settings.Default["EmailAddress"].ToString() == "" || Properties.Settings.Default["CustomerName"].ToString() == "")
+                if (startup == true && Convert.ToBoolean(Properties.Settings.Default["CheckForUpdates"]) == true)
+                {
+                    checkUpdate();
+                }
+                if (Properties.Settings.Default["EmailAddress"].ToString() == "" || Properties.Settings.Default["CustomerName"].ToString() == "")
+                {
+                    MessageBox.Show("Please Click On User Settings to Setup App Before Ordering", "Metro Order App", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnOrder.Enabled = false;
+                }
+                else
+                {
+                    btnOrder.Enabled = true;
+                }
+            } catch (System.Exception ex)
             {
-                MessageBox.Show("Please Click On User Settings to Setup App Before Ordering", "Metro Order App", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                btnOrder.Enabled = false;
-            }
-            else
-            {
-                btnOrder.Enabled = true;
+                MessageBox.Show(ex.Message, "Main Menu", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -109,28 +129,49 @@ namespace Order_App
         //OPEN UPDATE STOCK FILE FORM
         private void btnUpdate_Click(object sender, EventArgs e)
         {
-            Form3 form3 = new Form3();
-            form3.Show();
-            startup = false;
-            this.Hide();
+            try
+            {
+                Form3 form3 = new Form3();
+                form3.Show();
+                startup = false;
+                this.Hide();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Main Menu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         //OPEN USER SETTINGS FORM
         private void btnSettings_Click(object sender, EventArgs e)
         {
-            Form6 form6 = new Form6();
-            form6.Show();
-            startup = false;
-            this.Hide();
+            try
+            {
+                Form6 form6 = new Form6();
+                form6.Show();
+                startup = false;
+                this.Hide();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Main Menu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         //EXIT APPLICATION
         private void btnExit_Click(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("Do you really want to exit?", "Main Menu", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
+            try
             {
-                Application.Exit();
+                DialogResult result = MessageBox.Show("Do you really want to exit?", "Main Menu", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Main Menu", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
